a6309015997b3c6e87768a59ecd5ed0cb9140208
MonsterBasket <blazza1@hotmail.com>
2022-08-27 08:49:47 +1000

    allllmost did it entirely from memory

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import { useEffect, useState } from "react";
+
+function App(){
+    const [loading, setLoading] = useState(true);
+    const [image, setImage] = useState("");
+
+    useEffect(_ => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            setImage(json.message);
+            setLoading(false);
+        })
+    }, [])
+
+    return <div>
+        {loading ? <p>Loading...</p> : null}
+        {image ? <img src={image} alt="A Random Dog"></img> : null}
+    </div>
+
+}
+
+export default App;
